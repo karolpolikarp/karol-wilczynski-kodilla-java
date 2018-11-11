a7a8af58b15cd5b7651eaf77229b90a5d3113e06
124 <123@MacBook-Pro-Karol-2.local>
2018-11-11 18:17:26 +0100

    Zadanie 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,16 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
 import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.person.People;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.time.LocalDate;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -17,5 +26,33 @@ public class StreamMain {
 
         System.out.println("Using Stream to generate even numbers from 1 to 20");
         NumbersGenerator.generateEven(20);
+
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(System.out::println);
+
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n", "<<", ">>"));
+
+        System.out.println(theResultStringOfBooks);
+
+        System.out.println(" ");
+
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> mapOfForumUsers = forum.getUserList().stream()
+                .filter(forumUser -> forumUser.getUserSex()=='M')
+                .filter(forumUser -> forumUser.getUserDateOfBirth().isBefore(LocalDate.now().minusYears(20)))
+                .filter(forumUser -> forumUser.getUserPostCount() >= 1)
+                .collect(Collectors.toMap(ForumUser::getUserId, forumUser -> forumUser));
+
+        mapOfForumUsers.entrySet().stream()
+                .map(entry -> "UserId: " + entry.getKey() + ": UserData: " + entry.getValue())
+                .forEach(System.out::println);
     }
 }
