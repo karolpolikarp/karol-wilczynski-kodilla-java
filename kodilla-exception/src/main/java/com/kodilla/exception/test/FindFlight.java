2b3bc1f6863af9a43128c1f88e5b92282190169b
124 <123@MacBook-Pro-Karol-2.local>
2018-11-18 20:25:03 +0100

    Zadanie 8.4 (in progress)

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlight.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlight.java
@@ -7,12 +7,12 @@ public class FindFlight {
 
     public final void findFlight(Flight flight) throws RouteNotFoundException {
 
-        Map<String, Boolean> airportAvabilityChecker = new HashMap<>();
-        airportAvabilityChecker.put("Berlin", true);
-        airportAvabilityChecker.put("Paris", true);
-        airportAvabilityChecker.put("Haiti", false);
-        airportAvabilityChecker.put("London", true);
-        airportAvabilityChecker.put("Honolulu", false);
+        Map<String, Boolean> airportAvailabilityChecker = new HashMap<>();
+        airportAvailabilityChecker.put("Berlin", true);
+        airportAvailabilityChecker.put("Paris", true);
+        airportAvailabilityChecker.put("Haiti", false);
+        airportAvailabilityChecker.put("London", true);
+        airportAvailabilityChecker.put("Honolulu", false);
 
 
 }
